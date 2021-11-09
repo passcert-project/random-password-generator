2d07e7da4b95f23a341861567aac3583243d035a
badjelix <miguel.ptfg@hotmail.com>
2021-11-09 21:38:27 +0000

    updatelist clean

--- EC/UpdateList.ec
+++ EC/UpdateList.ec
@@ -1,33 +1,29 @@
-require import AllCore List.
+require import AllCore List Ring IntDiv StdOrder.
+import Ring.IntID IntOrder.
 
 op update (x : 'a) (s : 'a list) (n : int) =
   if n < 0 then s else (if size s <= n then s else take n s ++ x :: drop (n+1) s).
 
-(* auxiliary lemma on drop size *)
 lemma drop_plus1_size (x:'a) (string:'a list) (n:int) :
   0 <= n =>
   n < size string =>
   size (drop (n+1) string) = size (drop n string) - 1.
 proof.
-smt.
-qed.  
+move => h1 h2.
+rewrite (drop_nth x n string).
+split. assumption. move => h3. assumption.
+by simplify.
+qed.
 
 
 lemma update_empty (x:'a) (n:int) :
   update x [] n = [].
 proof.
 case (n < 0).
-- move => ?.
-  rewrite /update.
-  rewrite H.
-  by simplify.
-- move => ?.
-  rewrite /update.
-  rewrite H.
-  simplify.
-  rewrite lezNgt.
-  rewrite H.
-  by simplify.
+- move => h1.
+  by rewrite /update h1 /=.
+- move => h1.
+  by rewrite /update h1 /= lezNgt h1 /=.
 qed.
 
 
@@ -37,17 +33,14 @@ proof.
 rewrite /update.
 case (n < 0).
 - trivial.
-- move => ?.
+- move => h1.
   case (size string <= n).
   + trivial.
-  + move => ?.
-    rewrite size_cat.
-    simplify.
-    rewrite (drop_plus1_size x string n).    
-    + by rewrite -lezNgt in H.
-      by rewrite -ltzNge in H0.
-    simplify.
-    smt. (*completar, nao é dificil*)   
+  + move => h2.
+    rewrite size_cat /= (drop_plus1_size x string n).    
+    + by rewrite -lezNgt in h1.
+      by rewrite -ltzNge in h2.
+    by rewrite /= -size_cat cat_take_drop.
 qed.
 
 
@@ -59,10 +52,10 @@ case (n < 0).
 - trivial.
 - case (size string <= n).
   + trivial.
-  + move => ? ?.
+  + move => h1 h2.
     rewrite -(drop_nth x n string).
     split.
-    * by rewrite -lezNgt in H0.
-    * by rewrite -ltzNge in H.
+    * by rewrite -lezNgt in h2.
+    * by rewrite -ltzNge in h1.
     apply cat_take_drop.
 qed.
--- Tests/tests.c
+++ Tests/tests.c
@@ -1,10 +1,190 @@
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
-#include <inttypes.h>
+#include <string.h>
+#include <time.h>
 
-extern uint64_t generate_password(uint64_t policyAddr, uint64_t outputAddr);
+#define NTESTS 200
 
 int main(int argc, char *argv[]) {
 
+    FILE *fp;
+
+    // Bitwarden
+    if(strcmp(argv[1],"bw") == 0) {
+        if(strcmp(argv[2],"pin") == 0) {
+            fp = fopen("./output/bw_time_pin.txt","w+");
+            clock_t start, end;
+            for(int i=0; i<NTESTS; i++) {
+                start = clock();
+                system("bw generate --length 4 -n > /dev/null");
+                end = clock();
+                float seconds = (float)(end - start) / CLOCKS_PER_SEC;
+                fprintf(fp,"%f\n",seconds);
+            }
+            fclose(fp);
+        } else if(strcmp(argv[2],"2class12") == 0) {
+            fp = fopen("./output/bw_time_2class12.txt","w+");
+            clock_t start, end;
+            for(int i=0; i<NTESTS; i++) {
+                start = clock();
+                system("bw generate --length 12 -lu > /dev/null");
+                end = clock();
+                float seconds = (float)(end - start) / CLOCKS_PER_SEC;
+                fprintf(fp,"%f\n",seconds);
+            }
+            fclose(fp);
+        } else if(strcmp(argv[2],"3class12") == 0) {
+            fp = fopen("./output/bw_time_3class12.txt","w+");
+            clock_t start, end;
+            for(int i=0; i<NTESTS; i++) {
+                start = clock();
+                system("bw generate --length 12 -lun > /dev/null");
+                end = clock();
+                float seconds = (float)(end - start) / CLOCKS_PER_SEC;
+                fprintf(fp,"%f\n",seconds);
+            }
+            fclose(fp);
+        } else if(strcmp(argv[2],"2class16") == 0) {
+            fp = fopen("./output/bw_time_2class16.txt","w+");
+            clock_t start, end;
+            for(int i=0; i<NTESTS; i++) {
+                start = clock();
+                system("bw generate --length 16 -lu > /dev/null");
+                end = clock();
+                float seconds = (float)(end - start) / CLOCKS_PER_SEC;
+                fprintf(fp,"%f\n",seconds);
+            }
+            fclose(fp);
+        }
+
+    // Jasmin
+    } else if(strcmp(argv[1],"jasmin") == 0) {
+        if(strcmp(argv[2],"pin") == 0) {
+            fp = fopen("./output/jasmin_time_pin.txt","w+");
+            clock_t start, end;
+            for(int i=0; i<NTESTS; i++) {
+                start = clock();
+                system("node ../../cli/build/bw.js generate --jasminPolicy 4,0,0,0,0,4,4,0,0 > /dev/null");
+                end = clock();
+                float seconds = (float)(end - start) / CLOCKS_PER_SEC;
+                fprintf(fp,"%f\n",seconds);
+            }
+            fclose(fp);
+        } else if(strcmp(argv[2],"2class12") == 0) {
+            fp = fopen("./output/jasmin_time_2class12.txt","w+");
+            clock_t start, end;
+            for(int i=0; i<NTESTS; i++) {
+                start = clock();
+                system("node ../../cli/build/bw.js generate --jasminPolicy 12,0,12,0,12,0,0,0,0 > /dev/null");
+                end = clock();
+                float seconds = (float)(end - start) / CLOCKS_PER_SEC;
+                fprintf(fp,"%f\n",seconds);
+            }
+            fclose(fp);
+        } else if(strcmp(argv[2],"3class12") == 0) {
+            fp = fopen("./output/jasmin_time_3class12.txt","w+");
+            clock_t start, end;
+            for(int i=0; i<NTESTS; i++) {
+                start = clock();
+                system("node ../../cli/build/bw.js generate --jasminPolicy 12,0,12,0,12,0,12,0,0 > /dev/null");
+                end = clock();
+                float seconds = (float)(end - start) / CLOCKS_PER_SEC;
+                fprintf(fp,"%f\n",seconds);
+            }
+            fclose(fp);
+        } else if(strcmp(argv[2],"2class16") == 0) {
+            fp = fopen("./output/jasmin_time_2class16.txt","w+");
+            clock_t start, end;
+            for(int i=0; i<NTESTS; i++) {
+                start = clock();
+                system("node ../../cli/build/bw.js generate --jasminPolicy 16,0,16,0,16,0,0,0,0 > /dev/null");
+                end = clock();
+                float seconds = (float)(end - start) / CLOCKS_PER_SEC;
+                fprintf(fp,"%f\n",seconds);
+            }
+            fclose(fp);
+        }
+    // All
+    } else if (strcmp(argv[1],"all") == 0) {
+        fp = fopen("./output/bw_time_pin.txt","w+");
+        clock_t start, end;
+        for(int i=0; i<NTESTS; i++) {
+            start = clock();
+            system("bw generate --length 4 -n > /dev/null");
+            end = clock();
+            float seconds = (float)(end - start) / CLOCKS_PER_SEC;
+            fprintf(fp,"%f\n",seconds);
+        }
+        fclose(fp);
+
+        fp = fopen("./output/bw_time_2class12.txt","w+");
+        for(int i=0; i<NTESTS; i++) {
+            start = clock();
+            system("bw generate --length 12 -lu > /dev/null");
+            end = clock();
+            float seconds = (float)(end - start) / CLOCKS_PER_SEC;
+            fprintf(fp,"%f\n",seconds);
+        }
+        fclose(fp);
+
+        fp = fopen("./output/bw_time_3class12.txt","w+");
+        for(int i=0; i<NTESTS; i++) {
+            start = clock();
+            system("bw generate --length 12 -lun > /dev/null");
+            end = clock();
+            float seconds = (float)(end - start) / CLOCKS_PER_SEC;
+            fprintf(fp,"%f\n",seconds);
+        }
+        fclose(fp);
+
+        fp = fopen("./output/bw_time_2class16.txt","w+");
+        for(int i=0; i<NTESTS; i++) {
+            start = clock();
+            system("bw generate --length 16 -lu > /dev/null");
+            end = clock();
+            float seconds = (float)(end - start) / CLOCKS_PER_SEC;
+            fprintf(fp,"%f\n",seconds);
+        }
+        fclose(fp);
+
+        fp = fopen("./output/jasmin_time_pin.txt","w+");
+        for(int i=0; i<NTESTS; i++) {
+            start = clock();
+            system("node ../../cli/build/bw.js generate --jasminPolicy 4,0,0,0,0,4,4,0,0 > /dev/null");
+            end = clock();
+            float seconds = (float)(end - start) / CLOCKS_PER_SEC;
+            fprintf(fp,"%f\n",seconds);
+        }
+        fclose(fp);
+
+        fp = fopen("./output/jasmin_time_2class12.txt","w+");
+        for(int i=0; i<NTESTS; i++) {
+            start = clock();
+            system("node ../../cli/build/bw.js generate --jasminPolicy 12,0,12,0,12,0,0,0,0 > /dev/null");
+            end = clock();
+            float seconds = (float)(end - start) / CLOCKS_PER_SEC;
+            fprintf(fp,"%f\n",seconds);
+        }
+        fclose(fp);
+
+        fp = fopen("./output/jasmin_time_3class12.txt","w+");
+        for(int i=0; i<NTESTS; i++) {
+            start = clock();
+            system("node ../../cli/build/bw.js generate --jasminPolicy 12,0,12,0,12,0,12,0,0 > /dev/null");
+            end = clock();
+            float seconds = (float)(end - start) / CLOCKS_PER_SEC;
+            fprintf(fp,"%f\n",seconds);
+        }
+        fclose(fp);
+
+        fp = fopen("./output/jasmin_time_2class16.txt","w+");
+        for(int i=0; i<NTESTS; i++) {
+            start = clock();
+            system("node ../../cli/build/bw.js generate --jasminPolicy 16,0,16,0,16,0,0,0,0 > /dev/null");
+            end = clock();
+            float seconds = (float)(end - start) / CLOCKS_PER_SEC;
+            fprintf(fp,"%f\n",seconds);
+        }
+        fclose(fp);
+    }
 }
